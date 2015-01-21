5a49937197ee327f22da67c38294d7a4ddf9d862
yibuyisheng <yibuyisheng@163.com>
2015-01-21 20:41:46 +0800

    bug fix

--- static/js/src/ui/touchload.js
+++ static/js/src/ui/touchload.js
@@ -28,55 +28,88 @@ define([
                     this._$scroll.insertBefore(this._$top, this._$scroll.firstElementChild);
                     this._$scroll.appendChild(this._$bottom);
 
-                    this._hm = new Hammer(this._$scroll);
-                    this._hm.get('pan').set({direction: Hammer.DIRECTION_VERTICAL});
-                    var _this = this;
                     var state = {
-                        isTopStart: false,
-                        isBottomStart: false
+                        isStart: false,
+                        position: null, // 给当前滑动定性，究竟是变动顶部的div还是变动底部的div
+                        previousY: null
                     };
-                    this._hm.on('pandown', function(event) {
-                        if (_this._$scroll.scrollTop === 0) {
-                            state.isTopStart = true;
-                            _this._$top.style.display = 'block';
+                    var _this = this;
+                    this._$scroll.addEventListener('touchstart', touchstart);
+                    this._$scroll.addEventListener('touchmove', touchmove);
+                    this._$scroll.addEventListener('touchend', touchend);
+
+                    this._touchstart = touchstart;
+                    this._touchmove = touchmove;
+                    this._touchend = touchend;
+
+                    function touchstart() {
+                        state.isStart = true;
+                        state.previousY = event.touches[0].clientY;
+                    }
+
+                    function touchmove() {
+                        if (!state.isStart) return;
+
+                        _this._$top.style.display = 'block';
+                        _this._$bottom.style.display = 'block';
+
+                        var distance = event.touches[0].clientY - state.previousY;
+                        // 第一次进来，根据这次的运动方向决定position
+                        if (!state.position) {
+                            state.position = distance > 0 ? 'top' : 'bottom';
                         }
-                        if (!state.isTopStart) return;
 
-                        _this._$top.style.height = event.distance + 'px';
-                    });
-                    this._hm.on('panup', function(event) {
-                        if (_this._$scroll.scrollTop + _this._$scroll.offsetHeight === _this._$scroll.scrollHeight) {
-                            state.isBottomStart = true;
-                            _this._$bottom.style.display = 'block';
+                        // 应该顶部动
+                        if (state.position === 'top' && _this._$scroll.scrollTop === 0) {
+                            var preHeight = parseFloat(_this._$top.style.height);
+                            preHeight = preHeight ? preHeight : 0;
+                            _this._$top.style.height = (preHeight + distance) + 'px';
                         }
-                        if (!state.isBottomStart) return;
+                        // 应该底部动
+                        else if (state.position === 'bottom'
+                            && (_this._$scroll.scrollTop + _this._$scroll.offsetHeight === _this._$scroll.scrollHeight)
+                        ) {
+                            var preHeight = parseFloat(_this._$bottom.style.height);
+                            preHeight = preHeight ? preHeight : 0;
+                            _this._$bottom.style.height = (preHeight - distance) + 'px';
+                        }
 
-                        _this._$bottom.style.height = event.distance + 'px';
-                    });
-                    this._hm.on('panend', function(event) {
-                        _this._$top.style.display = 'none';
-                        _this._$top.style.height = '';
+                        state.previousY = event.touches[0].clientY;
+                    }
 
-                        _this._$bottom.style.display = 'none';
-                        _this._$bottom.style.height = '';
-
+                    function touchend() {
                         try {
-                            if (event.distance > 50) {
-                                state.isTopStart && _this.trigger('topLoad');
-                                state.isBottomStart && _this.trigger('bottomLoad');
+                            if (_this._$top.offsetHeight > 50) {
+                                _this.trigger('topLoad');
+                            } else if (_this._$bottom.offsetHeight > 50) {
+                                _this.trigger('bottomLoad');
                             }
                         } catch (e) {
                             throw e;
                         } finally {
-                            state.isTopStart = false;
-                            state.isBottomStart = false;
+                            state.isStart = false;
+                            state.previousY = null;
+                            state.position = null;
+
+                            _this._$top.style.display = 'none';
+                            _this._$top.style.height = '';
+                            _this._$bottom.style.display = 'none';
+                            _this._$bottom.style.height = '';
                         }
-                    });
+                    }
                 },
                 destroy: function() {
                     this._$scroll.removeChild(this._$top);
                     this._$scroll.removeChild(this._$bottom);
-                    this._hm.destroy();
+                    this._$scroll.removeEventListener('touchstart', this._touchstart);
+                    this._$scroll.removeEventListener('touchmove', this._touchmove);
+                    this._$scroll.removeEventListener('touchend', this._touchend);
+                },
+                scrollToTop: function() {
+                    this._$scroll.scrollTop = 0;
+                },
+                scrollToBottom: function() {
+                    this._$scroll.scrollTop = this._$scroll.scrollHeight - this._$scroll.offsetHeight;
                 }
             });
         }
