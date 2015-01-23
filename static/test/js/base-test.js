80bfa0dbfd0671e2c201423fc61cafd58fa60fb0
yibuyisheng <yibuyisheng@163.com>
2015-01-23 17:00:25 +0800

    base test

--- static/test/js/base-test.js
+++ static/test/js/base-test.js
@@ -12,12 +12,26 @@ describe('base.js测试', function() {
         equal('c', a.b.c);
     });
 
-    it('bind', function() {
+    it('bind给函数绑定this', function() {
         var obj = {a: 1};
         function fn() {
             equal(1, this.a);
         }
 
         WEBUI.base.bind(fn, obj)();
+    });
+
+    it('format', function() {
+        equal('hello world!', WEBUI.base.format('hello {0}{1}', 'world', '!'));
+    });
+
+    it('keys and values', function() {
+        var obj = {
+            a: 1,
+            b: 2,
+            c: 3
+        };
+        equal('a', WEBUI.base.keys(obj).sort()[0]);
+        equal(1, WEBUI.base.values(obj).sort()[0]);
     });
 });
