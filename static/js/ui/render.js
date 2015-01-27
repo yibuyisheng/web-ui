5d82d495e68c4780ea9826109994251d27d8cc99
yibuyisheng <yibuyisheng@163.com>
2015-01-27 14:18:07 +0800

    封装

--- static/js/ui/render.js
+++ static/js/ui/render.js
@@ -2,75 +2,114 @@
  * 渲染html
  */
 
-var nodeEnum = {
-    ELEMENT_NODE: 1,
-    TEXT_NODE: 3
-};
-var EXPRESSION_REGEXP = /\{\{(.*?)\}\}/g;
 (function(global) {
-    var tpl = [
-        '<div>',
-            '<span class="{{klass}}">{{klass}}</span>',
-        '<div>'
-    ].join('');
 
-
-    window.onload = function() {
-        var dom = createDom(tpl);
-        var expressions = collectExpressions(dom);
-        var cloneDom = createDom(dom.innerHTML);
-        update(cloneDom, expressions, {klass: 'test'});
-        document.body.appendChild(cloneDom);
+    var nodeEnum = {
+        ELEMENT_NODE: 1,
+        TEXT_NODE: 3
     };
+    var EXPRESSION_REGEXP = /\{\{(.*?)\}\}/g;
+
+    // 注册的tag
+    var tags = {};
+
+    global.render = {
+        createTag: createTag,
+        mount: mount
+    };
+
+    function createTag(name, tpl) {
+        tags[name] = tpl;
+    }
+
+    function mount(name, vm) {
+        var nodes = document.getElementsByTagName(name);
+        var tpl = tags[name];
+        if (!tpl) return;
+
+        var instances = [];
+        for (var i = 0, il = nodes.length; i < il; i++) {
+            var dom = createDom(tpl);
+            var expressions = collectExpressions(dom);
+            var instance = {
+                vm: vm,
+                expressions: expressions,
+                update: function() {
+                    update(this.expressions, this.vm);
+                }
+            };
+            instance.update();
+            instances.push(instance);
+
+            nodes[i].parentNode.replaceChild(dom, nodes[i]);
+        }
+        return instances;
+    }
 
     function createDom(tpl) {
         var div = document.createElement('div');
         div.innerHTML = tpl;
-        return div;
+        if (div.children.length !== 1) {
+            throw new Error('tag must have one element root');
+        }
+        return div.firstElementChild;
     }
 
-    function generateUuid() {
-        generateUuid.id = generateUuid.id || 0;
-        return generateUuid.id++;
-    }
-
-    // 搜集表达式
+    /**
+     * 搜集表达式
+     * expressions数据结构：
+     * {
+     *     '{{name}}': {
+     *         list: [{node: node, updateFn: function(value, node) {...}}], // 节点及更新节点的方法
+     *         fnObj: {                                                     // 重新计算表达式值的函数
+     *             args: [...],                                             // 此函数的参数名字数组（一个字符串数组）
+     *             fn: function() {...}                                     // 具体的函数
+     *         }
+     *     }
+     * }
+     */
     function collectExpressions(dom) {
         var expressions = {};
-        tracerse(dom, function(node) {
+        traverse(dom, function(node) {
             if (node.nodeType === nodeEnum.ELEMENT_NODE) {
-                var uniqueId = generateUuid();
-                node.setAttribute('unique-id', uniqueId);
+                // if (node.getAttribute('repeat')) {
+                //     var cmtStart = document.createComment('start: ' + node.outerHTML);
+                //     var cmtEnd = document.createComment('end');
+                //     var parentNode = node.parentNode;
+                //     parentNode.replaceChild(cmtStart, node);
+                //     parentNode.insertBefore(cmtEnd, cmtStart.nextSibling);
+                //     add(node.outerHTML, [cmtStart, cmtEnd, node], function(value, opts) {
+                //         var cmtStart = opts[0];
+                //         var cmtEnd = opts[1];
+                //         var node = opts[2];
+                //     });
+                //     return;
+                // }
 
                 var attrs = node.attributes;
                 for (var i = 0, il = attrs.length; i < il; i++) {
                     var attr = attrs[i];
-                    if (EXPRESSION_REGEXP.test(attr.value)) {
-                        add(attr.value, uniqueId, function(value, uniqueId, rootNode, opts) {
-                            var node = rootNode.querySelector('*[unique-id="' + uniqueId + '"]');
-                            node.setAttribute(opts.attrName, value);
-                        }, {
-                            attrName: attr.name
-                        });
-                    }
+                    if (!EXPRESSION_REGEXP.test(attr.value)) continue;
+                    add(attr.value, node, function(value, node) {
+                        node.setAttribute(attr.name, value);
+                    });
                 }
-            } else if (node.nodeType === nodeEnum.TEXT_NODE) {
-                // if (EXPRESSION_REGEXP.test(node.textContent)) {
-                //     add(node.textContent, uniqueId.parentNode.getAttribute('unique-id'), function(value, uniqueId, rootNode) {
-                //         var parentNode = rootNode.querySelector('*[unique-id="' + uniqueId + '"]');
-                //         node.textContent = value;
-                //     });
-                // }
+            } else if (
+                node.nodeType === nodeEnum.TEXT_NODE
+                && EXPRESSION_REGEXP.test(node.textContent)
+            ) {
+                add(node.textContent, node, function(value, node) {
+                    node.textContent = value;
+                });
             }
         });
         return expressions;
 
-        function add(expression, uniqueId, updateFn, opts) {
+        function add(expression, opts, updateFn) {
             expressions[expression] = expressions[expression] || {list: []};
             expressions[expression].list.push({
-                uniqueId: uniqueId,
-                updateFn: updateFn,
-                opts: opts
+                opts: opts,
+                updateFn: updateFn
             });
             expressions[expression].fnObj = expressions[expression].fnObj || createFn(expression);
         }
@@ -90,10 +129,13 @@ var EXPRESSION_REGEXP = /\{\{(.*?)\}\}/g;
     }
 
     // 遍历节点
-    function tracerse(dom, fn) {
+    // filterFn用于过滤node，如果返回值为false，则不再遍历该节点下面的子孙节点
+    function traverse(dom, fn, filterFn) {
         var stack = [dom];
         while (stack.length) {
             var top = stack.pop();
+            fn(top);
+            if (filterFn && !filterFn(top)) continue;
             var childNodes = top.childNodes;
             for (var i = 0, il = childNodes.length; i < il; i += 1) {
                 fn(childNodes[i]);
@@ -103,7 +145,7 @@ var EXPRESSION_REGEXP = /\{\{(.*?)\}\}/g;
     }
 
     // 将数据渲染上去
-    function update(dom, expressions, vm) {
+    function update(expressions, vm) {
         for (var expr in expressions) {
             var exprObj = expressions[expr];
             var params = [];
@@ -115,7 +157,7 @@ var EXPRESSION_REGEXP = /\{\{(.*?)\}\}/g;
             // 脏检测
             if (exprValue !== exprObj.lastValue) {
                 for (var i in exprObj.list) {
-                    exprObj.list[i].updateFn(exprValue, exprObj.list[i].uniqueId, dom, exprObj.list[i].opts);
+                    exprObj.list[i].updateFn(exprValue, exprObj.list[i].node);
                 }
             }
             exprObj.lastValue = exprValue;
--- static/test/js/render-test.js
+++ static/test/js/render-test.js
@@ -1,5 +1,17 @@
 describe('render.js', function() {
     it('test', function() {
+        var tpl = '<div class="{{name}}">{{name}}<ul><li repeat="i in items"></li></ul></div>';
+        WEBUI.render.createTag('test', tpl);
+        var instances = WEBUI.render.mount('test', {
+            name: 'zhangsan',
+            items: ['a', 'b', 'c']
+        });
 
+        var counter = 0;
+        document.getElementById('update').addEventListener('click', function() {
+            var instance = instances[0];
+            instance.vm = {name: 'haha' + counter++};
+            instance.update();
+        });
     });
 });
